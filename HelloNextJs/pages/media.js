b37386e6dcf6c1092cc512d1c4a54a9258757a5b
toandx <strangersuntoan@gmail.com>
2022-09-08 22:13:11 +0700

    update code

---
+++ HelloNextJs/pages/media.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link';
+
+export default function Media() {
+  return (
+    <div>
+      <h1> Media page </h1>
+      <Link href="/">Home page!</Link>
+    </div>
+  )
+}
